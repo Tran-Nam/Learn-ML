10ac225abf80443bf77e9b972b594707f715d86d
Tran-Nam <phuongnam9613@gmail.com>
2019-01-02 16:16:03 +0700

    complete

--- keras/RNN_example.py
+++ keras/RNN_example.py
@@ -5,7 +5,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('./data', one_hot=True)
 
 lr = 1e-3
-training_step = 10000
+training_step = 20000
 batch_size = 128
 display_step = 200                                
 
@@ -22,5 +22,47 @@ weights = {
 }
 
 bias = {
-     
+     'out': tf.Variable(tf.random_normal([num_class]))
 }
+
+def RNN(x, weights, bias):
+    x = tf.unstack(x, timesteps, 1)
+    lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.)
+    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+    return tf.matmul(outputs[-1], weights['out']) + bias['out']
+
+logits = RNN(X, weights, bias)
+prediction = tf.nn.softmax(logits)
+
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
+train_op = optimizer.minimize(loss_op)
+
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for step in range(1, training_step+1):
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+        batch_x = batch_x.reshape((batch_size, timesteps, num_input))
+
+        sess.run(train_op, {X: batch_x, Y: batch_y})
+
+        if step%display_step == 0 or step == 1:
+            loss, acc = sess.run([loss_op, accuracy], {X:batch_x, Y: batch_y})
+
+            print("Step " + str(step) + ", Minibatch Loss " + \
+            "{:.4f}".format(loss)+ ", Training Accuracy: " + \
+            "{:.3f}".format(acc))
+
+    print("Optimization Finished!")
+
+    test_len = 128
+    test_data = mnist.test.images[:test_len].reshape((-1, timesteps, num_input))
+    test_label = mnist.test.labels[:test_len]
+    print("Testing Accuracy: ", \
+    sess.run(accuracy, {X: test_data, Y: test_label}))
